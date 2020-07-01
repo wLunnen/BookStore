6a41360be9bd1f009a7c74915848e0bd1066bc85
Wesley Lunnen <wesley.lunnen@gmail.com>
2020-07-01 12:07:19 -0500

    first commit

---
+++ BookStore/BookStore.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookStore.UI.MVC.Startup))]
+namespace BookStore.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
